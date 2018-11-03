c9366f4cd70aa1c06ca8a2bf2ee1bf152c33ccde
Melisse Doroteo <gabbie_doroteo@yahoo.com>
2018-11-03 01:06:45 -0600

    offer rides like 90% ok i guess
    i guess :(

--- rides.py
+++ rides.py
@@ -17,47 +17,49 @@ def offerRide(cursor, conn, email):
     validSeatPrice(seatPrice)
 
     luggage = input("Enter luggage description: ") #fix luggage desc?????
-
+    print("Source Location")
     sourceLoc = getLoc(cursor, conn, email)
+    print("Destination Location")
     destLoc = getLoc(cursor, conn, email)
+    carNum = 'NULL' #if did not want to enter a car num
     askCarNum = input("Would you like to add a car number? [Y/N]")
     if askCarNum.upper() == 'Y':
         carNum = getCarNum(cursor, conn, email)
-        
-    elif askCarNum.upper() == 'N':
-        enrouteSet = []
-        askEnroute = input("Would you like to add an enroute location? [Y/N]")
-        if askEnroute.upper() == 'Y':
-            addEnroute = getLoc(cursor, conn)
-            enrouteSet.append(addEnroute)
-            askEnrouteAgain = input ("Would you like to add another enroute location? [Y/N] ")
 
-    rideNum = getRideNum(cursor, conn)
+    # elif askCarNum.upper() == 'N':
+    #     enrouteSet = []
+    #     askEnroute = input("Would you like to add an enroute location? [Y/N]")
+    #     if askEnroute.upper() == 'Y':
+    #         addEnroute = getLoc(cursor, conn)
+    #         enrouteSet.append(addEnroute)
+    #         askEnrouteAgain = input ("Would you like to add another enroute location? [Y/N] ")
 
-    # enrouteSet = []
-    # askEnroute = input("Would you like to add an enroute location? [Y/N]")
-    # if askEnroute.upper() == 'Y':
-    #     addEnroute = getLoc(cursor, conn)
-    #     enrouteSet.append(addEnroute)
-    #     askEnrouteAgain = input ("Would you like to add another enroute location? [Y/N] ")
-    #     #if askEnrouteAgain.upper() == 'Y':
-            ########fix this move this somewhere???
+    #rideNum = getRideNum(cursor, conn)
+
+    enrouteSet = []
+    askEnroute = input("Would you like to add an enroute location? [Y/N]")
+    if askEnroute.upper() == 'Y':
+        addEnroute = getLoc(cursor, conn, email)
+        enrouteSet.append(addEnroute)
+        askEnrouteAgain = input ("Would you like to add another enroute location? [Y/N] ")
+        #if askEnrouteAgain.upper() == 'Y':
+            #######fix this move this somewhere???
     # elif askEnroute.upper() == 'N':
     #     return
-
+    assignRideNum = getRideNum(cursor, conn, email)
     if len(enrouteSet) != 0:
         for loc in enrouteSet:
-            cursor.execute("INSERT INTO enroute values (?, ?)", (rideNum, loc))
+            cursor.execute("INSERT INTO enroute values (?, ?)", (assignRideNum, loc))
             connection.commit()
-    cursor.execute("INSERT INTO rides values (?, ?, ?, ?, ?, ?, ?, ?, ?)", (rideNum, seatPrice, date, numSeats, luggage, sourceLoc, destLoc, carNum))
+    cursor.execute("INSERT INTO rides values (?, ?, ?, ?, ?, ?, ?, ?, ?)", (assignRideNum, seatPrice, date, numSeats, luggage, sourceLoc, destLoc, email, carNum))
     connection.commit()
     askAddAnotherRide = input("Would you like to add another ride? [Y/N] ")
     if askAddAnotherRide.upper() == Y:
         offer(cursor, connection, email)
-    return
+    return #???????
 
 
-def checkValidDate(date):
+def checkValidDate(date): #fix to check if date is in future
     date = date
     date1 = date.split('-')
     #print(date1)
@@ -84,11 +86,39 @@ def validSeatPrice(seatPrice):
     else:
         return True
 
-def getLoc(cursor, conn, email):
-    #finshhhhhhhhhh#########
+def getLoc(cursor, conn, email): #fix so can find location if keyword not a lcode
     location = input("Enter a location code or keyword to search: ")
     cursor.execute("SELECT city, prov from locations where ? = lcode", (location.lower(),))
-
+    location = '%' + location.lower() + '%'
+    found = cursor.fetchone()
+    if found != None:
+        confirm = input("Location " + found[0] + " " + found[1] + ". Is this correct? [Y/N]")
+        if confirm.upper() == 'Y':
+            return location.lower()
+        else:
+        	return getLoc(cursor, conn, email)
+    else:
+        cursor.execute("SELECT * from locations where (city like ? or prov like ? or address like ?) ", (location, location, location))
+        locSet = cursor.fetchall()
+        count = 0
+        loops = 0
+        for location in locSet:
+            print("Option " + str(count + 1) + ': ' + location[1] + " " + location[2] + " " + location[3])
+            if count == 4 or location == locSet[len(locSet) - 1]:
+                choice = input("Select one of these locations? (Enter option # or press enter to see more)")
+                if choice == '':
+                    count = -1
+                    loops +=1
+                else:
+                    try:
+                        choice = int(choice)
+                        return locSet[(loops * 4) + (choice - 1)][0]
+                    except ValueError:
+                        print("Invalid option. Please retry search.")
+                        return getLoc(cursor, conn, email)
+                count += 1
+        print("Location not found. Please try another location code or keyword")
+        return getLoc(cursor, conn, email)
 
 
 def getCarNum(cursor, conn, email):
@@ -102,8 +132,13 @@ def getCarNum(cursor, conn, email):
 
 
 
-#def getRideNum(cursor, conn, email)
-    ###finsih##########
+def getRideNum(cursor, conn, email):
+    cursor.execute("SELECT max(rno) from rides")
+    last = cursor.fetchone()
+    if last == None:
+        return 1
+    rno = last[0] + 1
+    return rno
 
 #def searchRides(cursor, conn, email)
     ####finish#########
