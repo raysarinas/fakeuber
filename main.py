8111e38f99711642a7840ccc1ae91de9a1a5db52
Raymond Sarinas <raysarinas97@gmail.com>
2018-11-05 14:23:45 -0700

    ,adfms

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import sqlite3
 from os import system, name
 import os.path
 from sys import exit
-#from tables import *
 from rides import *
 from bookings import *
 from riderequests import *
@@ -13,84 +12,54 @@ from clear import clear
 connection = None
 cursor = None
 
-# def clear():
-#     if name == 'nt':
-#         _ = system('cls')
-#     else:
-#         _ = system('clear')
-
 def exitProgram(conn):
     print("ok bye program exited")
     conn.commit()
     conn.close()
     exit()
 
-'''TEST FUNCTION TO TRY AND POPULATE DATABASE'''
-def populate(conn, cursor):
-    #data = open("prj-tables.sql", "r")
-    data = open('prj-data.sql').read()
-    cursor.executescript(data)
-    conn.commit()
-    # for line in data.readlines():
-    #     line = line.strip()
-        #print(line)
-        #cursor.execute(line)
-        #conn.commit()
-
 def main():
-    ''' initialize application or something'''
+    # initialize everything i guess
+    clear()
+    db = input('Enter a database to use (with .db extension): ') #"./uberDB.db"
+    #db = './uberDB.db'
+    check = os.path.isfile(db)
+    conn = sqlite3.connect(db) # creates or opens a db in that path
+    cursor = conn.cursor()
+
+    cursor.execute('PRAGMA foreign_keys=ON;') # set foreign key constraint
+
     clear()
     print('-----------------------------------------------------------')
     print('Welcome to UberLite')
     print('-----------------------------------------------------------')
     print('Login or Register a new user')
 
-    #TODO: PERHAPS CHANGE THIS SO THE DATABASE IS NOT HARDCODED IN??????
-    db = "./uberDB.db"
-    check = os.path.isfile(db)
-    conn = sqlite3.connect(db) # creates or opens a db in that path
-
-    # CHANGE ./movie.db TO DATABASE WE WILL USE FOR OUR DATA OR WHATEVER
-    cursor = conn.cursor()
-    #populate(conn, cursor)
-
-    if not check: #if db not exists, populate
-        populate(conn, cursor)
-
-    cursor.execute('PRAGMA foreign_keys=ON;') # set foreign key constraint
-    #create_tables(cursor, conn)
-
-    # TESTING
-    #cursor.execute(''' INSERT''')
-
     email = None
 
     while True:
+        # wait for user to select/enter to do something
+        print('Enter 1 or 2 to log in or register as a new user. ')
+        print('Otherwise, enter anything else to exit program. ')
         print('1 - Login')
         print('2 - Register New User')
-        print('3 - Exit Program')
-        try:
-            selection = int(input())
-        except ValueError:
-            print('Not a number. Do it again')
-            continue
+        selection = input()
         clear()
-        if selection == 1:
+        if selection == '1':
             email = getLogin(cursor, conn)
             break
-        elif selection == 2:
+        elif selection == '2':
             email = registerNewUser(cursor, conn)
             break
-        elif selection == 3:
-            exitProgram(conn)
         else:
-            print('Invalid Selection. Try Again')
-            continue
-    # DO LOGIN PAGE STUFF HERE???
+            exitProgram(conn)
+
+    # display functionalities
     systemFunctionalities(cursor, conn, email)
     exitProgram(conn) # close the database? exit program
 
 def systemFunctionalities(cursor, conn, email):
+    # display and allow user to pick different things to do in the program
     print('-----------------------------------------------------------')
     print('Watcha wanna do? Enter a number below to continue or anything else to exit')
     print('1 - offer a ride')
@@ -100,46 +69,58 @@ def systemFunctionalities(cursor, conn, email):
     print('5 - search and delete ride requests')
     print('6 - log out')
 
-    user_input = int(input())
+    user_input = input()
 
-    if (user_input == 1):
+    if (user_input == '1'):
         clear()
         offerRide(cursor, conn, email)
 
-    if (user_input == 2):
+    if (user_input == '2'):
         clear()
         searchRides(cursor, conn, email)
 
-    if (user_input == 3):
+    if (user_input == '3'):
         clear()
         getBookings(email, cursor, conn)
 
-    if (user_input == 4):
+    if (user_input == '4'):
         clear()
         postRequest(cursor, conn, email)
 
-    if (user_input == 5):
+    if (user_input == '5'):
         clear()
         searchDeleteRequest(cursor, conn, email)
 
-    if (user_input == 6):
+    if (user_input == '6'):
         logout()
         #exitProgram(conn)
 
-    if (user_input == 420):
+    if (user_input == "420"):
         print('blaze it')
 
     while True:
+        # ask if user wants to continue if input is wrong or incorrect
+        # also break out into this when user finished operating/using a certain functionality
         cont = str(input('Do you want to continue? \nEnter 1 to Continue, otherwise exit/logout: '))
 
         if cont is '1':
+            # if want to continue, clear screen and display functionality options again
             clear()
             systemFunctionalities(cursor, conn, email)
         else:
+            # otherwise, log out
             logout()
 
 def logout():
+    # log out just jumps out and restarts program
     main()
+
+def exitProgram(conn):
+    # exit the program and then close the database
+    print("ok bye program exited")
+    conn.commit()
+    conn.close()
+    exit()
 
 if __name__ == "__main__":
     main()
